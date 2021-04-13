3308ef7cb6251babac8eaf8d600546dd05f19618
clementpeleman <clementpeleman@outlook.com>
2021-04-13 13:18:31 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <a href="#">clement peleman</a>
+      <div className="header-right">
+        <a href="#contact">contact</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/State.js
@@ -0,0 +1,10 @@
+import {createRef} from "react";
+
+const State = {
+    sections: 2,
+    pages: 2,
+    zoom: 1,
+    top: createRef(),
+};
+
+export default State;
